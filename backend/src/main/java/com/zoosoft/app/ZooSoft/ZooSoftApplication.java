4ce9db66f7ec8d83fd788b3e4042a87d281c3011
= <=>
2025-10-26 21:36:29 +0530

    made some front and backend changes

---
+++ backend/src/main/java/com/zoosoft/app/ZooSoft/ZooSoftApplication.java
@@ -0,0 +1,13 @@
+package com.zoosoft.app.ZooSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooSoftApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/zoosoft/app/ZooSoft/ZooSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoosoft.app.ZooSoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
